fc06b562a63ef59b012da4e9a36225a2d90704b3
Jack Hessel <jmhessel@gmail.com>
2020-03-29 21:47:32 -0400

    more wrk

--- slurm/generate_slurm_popularity.py
+++ slurm/generate_slurm_popularity.py
@@ -1,4 +1,5 @@
 '''
+/home/jmh563/compiled_python3/bin/python3
 '''
 import argparse
 
@@ -25,7 +26,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-    
+
     for model in ['full']:
         for idx in range(15):
             for lr in [.000001,.000005,.00001,.00005,.0001]:
@@ -36,7 +37,24 @@ def main():
                     IDX=idx,
                     MODEL=model,
                     LR='{:.9f}'.format(lr))
-                rm_cmd = 'rm reddit_{MODEL}_{IDX}_{LR}/*.pth'.format(MODEL=model, IDX=idx, LR='{:.9f}'.format(lr))
+                rm_cmd = 'python src/clean_all_but_best.py reddit_{MODEL}_{IDX}_{LR}/'.format(MODEL=model, IDX=idx, LR='{:.9f}'.format(lr))
+                with open(fname, 'w') as f:
+                    f.write(cur_header + '\n\n')
+                    f.write('cd ..; ')
+                    f.write(cmd + '; ' )
+                    f.write(rm_cmd + '\n')
+
+    for model in ['full']:
+        for idx in range(15):
+            for lr in [.000001,.000005,.00001,.00005,.0001]:
+                fname = '{}logits_{}_{}.sub'.format(model, idx, lr)
+                cur_header = header.format(NAME='{}logits_{}_{}'.format(model, idx, lr))
+                cmd = '{PYTHON} src/tasks/rank.py data/reddit_data/pics_split_{IDX}.json.lxmert_train_json data/reddit_data/pics_split_{IDX}.json.lxmert_val_json data/reddit_data/pics_split_{IDX}.json.lxmert_test_json data/reddit_data/pics_bbox_features.tsv reddit_{MODEL}logits_{IDX}_{LR} --loadLXMERT snap/pretrained/model_LXRT.pth --model_type {MODEL} --lr {LR} --epochs 10 --optimize_metric acc --use_logits 1'.format(
+                    PYTHON=args.python_executable,
+                    IDX=idx,
+                    MODEL=model,
+                    LR='{:.9f}'.format(lr))
+                rm_cmd = 'python src/clean_all_but_best.py reddit_{MODEL}logits_{IDX}_{LR}/'.format(MODEL=model, IDX=idx, LR='{:.9f}'.format(lr))
                 with open(fname, 'w') as f:
                     f.write(cur_header + '\n\n')
                     f.write('cd ..; ')
